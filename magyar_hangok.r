549d6f26b50b401cd834b07dbfe60d7987085cd0
Alex Trickey <k.alex.trickey@gmail.com>
2017-10-29 11:30:38 -0700

    Adds basic io

--- magyar_hangok.r
+++ magyar_hangok.r
@@ -1,7 +1,6 @@
 #Libs
 library(tuneR)
 
-
 #Constants
 VOWELS = c(
   'a', 'a_accent',
@@ -12,7 +11,7 @@ VOWELS = c(
   )
 EQUIV = c('y','ly') #These are actually phoenetically equivalent?
 
-#Function to Check OS
+#Source: https://www.r-bloggers.com/identifying-the-os-from-r/
 get_os <- function(){
   sysinf <- Sys.info()
   if (!is.null(sysinf)){
@@ -29,11 +28,44 @@ get_os <- function(){
   tolower(os)
 }
 
+print_intro <- function() {
+  cat('\nWelcome to your Hungarian Perceptual Trainer!
+    The available letters are listed below. Use these letters/notations to
+    identify your selections at the prompt.\n\n')
+  cat(letter_set)
+  cat("\n\n    Press [enter] to begin (or 'ctrl+z' to exit).")
+  invisible(readLines("stdin",n=1))
+}
+
+letter_from_file <- function(sound_files) {
+  letter_set = unique(gsub('_[0-9].mp3','',sound_files))
+  return(letter_set)
+}
+
 #Change default audio player for macs
 if(get_os() == 'osx') {
     setWavPlayer('/usr/bin/afplay')
 }
 
-#play sound
-a1 = readMP3('sounds/a_1.mp3')
-play(a1)
+
+#Get Sound Files and Letters
+sound_files = list.files('sounds')
+letter_set = letter_from_file(sound_files)
+
+print_intro()
+
+for(i in sample(1:length(sound_files))) {
+
+  cat('...playing sound...')
+  s = readMP3(file.path('sounds',sound_files[i]))
+  play(s)
+
+  cat("Which sound did you here? Letter: ")
+  user_letter = invisible(readLines("stdin",n=1))
+  actual_letter = letter_from_file(sound_files[i])
+  cat(ifelse(user_letter==actual_letter,
+             'Correct!',
+             paste('Oops, shoud be:',actual_letter)))
+  cat("\nPress [enter] when ready for next sound!")
+  invisible(readLines("stdin",n=1))
+}

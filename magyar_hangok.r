a69ed476ced53a29a1036c2eeb9c914bffbf28ce
Alex Trickey <k.alex.trickey@gmail.com>
2017-10-29 18:24:27 -0700

    Use actual diacritics

--- magyar_hangok.r
+++ magyar_hangok.r
@@ -31,11 +31,28 @@ get_os <- function(){
   tolower(os)
 }
 
-print_intro <- function() {
+get_character_map <- function(subset=NA) {
+  #Load mapping between ascii/special characters
+  character_map = read.csv('character_map.csv',
+                           stringsAsFactors=FALSE,
+                           strip.white = TRUE)
+
+  #Build a lookup table
+  character_lookup = unlist(character_map['special'])
+  names(character_lookup) = unlist(character_map['ascii'])
+
+  #Get a list of all letters/formats
+  all_letters = unlist(union(character_map['ascii'],character_map['special']))
+
+  char = list('map'=character_map,'lookup'=character_lookup,'all'=all_letters)
+  return(char)
+}
+
+print_intro <- function(character_map) {
   cat('\nWelcome to your Hungarian Perceptual Trainer!
-    The available letters are listed below. Use these letters/notations to
-    identify your selections at the prompt.\n\n')
-  character_map = read.csv('character_map.csv')
+    The letters to be used in the following exercises
+    are listed below. You may use either the ascii or
+    special characters to respond to the prompt.\n\n')
   print(character_map)
   cat("\n\n    Press [enter] to begin (or 'ctrl+z' to exit).")
   invisible(readLines("stdin",n=1))
@@ -46,10 +63,32 @@ letter_from_file <- function(sound_files) {
   return(letter_set)
 }
 
-get_user_letter <- function() {
-  cat("Which sound did you here? Letter: ")
+get_user_letter <- function(letter_set) {
+
   user_letter = invisible(readLines("stdin",n=1))
+  while(!is.element(user_letter,letter_set)) {
+      cat('Unrecognized character, try again please: ')
+      user_letter = invisible(readLines("stdin",n=1))
+  }
   return(user_letter)
+}
+
+check_user_input <- function(user_letter,actual_letter,char) {
+
+  #Look up non-ascii form
+  actual_diacritic = char$lookup[actual_letter]
+  user_diacritic = char$lookup[user_letter]
+
+  #Check answer, give feedback
+  correct = actual_diacritic == user_diacritic
+  cat(ifelse(correct,'Correct!',
+             paste('Oops, shoud be:',actual_diacritic)))
+
+  #Return response for log
+  perf = matrix(c(sound_files[i],
+                  actual_diacritic,
+                  user_diacritic),nrow=1)
+  return(perf)
 }
 
 #Change default audio player for macs
@@ -57,49 +96,48 @@ if(get_os() == 'osx') {
     setWavPlayer('/usr/bin/afplay')
 }
 
-#Get Sound Files and Letters
+#Get Sound Files and Characters
 sound_files = list.files('sounds')
-letter_set = letter_from_file(sound_files)
-
-print_intro()
+char = get_character_map()
+print_intro(character_map=char$map)
 
 #initialize log df
 log = data.frame()
+
+#Loop over stimuli
 for(i in sample(1:length(sound_files))) {
 
-  cat('...playing sound...')
+  cat('...playing sound... What sound do you hear? Letter: ')
   s = readMP3(file.path('sounds',sound_files[i]))
   play(s)
 
-  user_letter = get_user_letter()
-  while(!is.element(user_letter,letter_set)) {
-    'Character not in letter set, try again please.'
-    user_letter = get_user_letter()
-  }
-
+  user_letter = get_user_letter(char$all)
   actual_letter = letter_from_file(sound_files[i])
-  correct = user_letter==actual_letter
-  cat(ifelse(correct,'Correct!',
-             paste('Oops, shoud be:',actual_letter)))
-  perf = matrix(c(sound_files[i],
-                  actual_letter,
-                  user_letter,
-                  as.numeric(correct)),nrow=1)
+
+  perf = check_user_input(user_letter,actual_letter,char)
+
   log = rbind(log,perf)
-  cat("\nPress [enter] when ready for next sound!")
+  cat("\nPress [enter] to continue.")
   invisible(readLines("stdin",n=1))
   break
 }
 
-names(log) = c('file','letter','response','correct')
-log['date'] = format(Sys.Date())
-log['ts'] = format(Sys.time())
+cat('Trials Complete!')
+cat('\nSave results to log file (y/n)?')
 
-cat('Save results to log file (y/n)?')
+names(log) = c('file','actual','response')
+log['correct'] = log['actual'] == log['response']
+
 write_to_log = invisible(readLines("stdin",n=1))
 if( !(write_to_log == 'n')) {
-  write.csv(log,file='performance.csv',append=TRUE)
+  if(!is.element('performance.csv',list.files())) {
+    log['date'] = format(Sys.Date())
+    log['ts'] = format(Sys.time())
+    header <- paste(names(log),collapse=',')
+    write.table(header, file='performance.csv', row.names=FALSE, col.names=FALSE, quote=FALSE)
+  }
+  write.table(log, file='performance.csv', row.names=FALSE, col.names=FALSE, append=TRUE, sep=",")
 }
-print('ok... and ... ???')
-cat(paste('Accuracy:',sum(log$correct)/length(log)))
-cat("Motivational message... blah blah... yay! keep going!")
+
+cat(paste('\n\nAccuracy:',100*sum(log$correct)/(dim(log)[1])))
+cat("\n Motivational message!\n \n ")
